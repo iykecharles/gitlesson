4db3d0c207fe0d87469847e52d78c0c141f5da2a
iykecharles <iykecharles316@yahoo.com>
2021-01-13 11:50:32 +0100

    revised main.go file commited

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h2>Yes, our website is online</h2>")
+}
 
 func main() {
-	fmt.Println("helloooooo")
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":3000", nil)
 }
