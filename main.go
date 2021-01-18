15af40e8eb1f1e0273fdfd476e5a858383aec5c0
iykecharles <iykecharles316@yahoo.com>
2021-01-18 10:49:41 +0100

    Added templates and modified the main.go file

--- main.go
+++ main.go
@@ -2,14 +2,43 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h2>Yes, our website is online</h2>")
-}
+var (
+	templates = template.Must(template.ParseGlob("templates/*"))
+)
 
 func main() {
-	http.HandleFunc("/", index)
+	r := mux.NewRouter()
+	r.NotFoundHandler = http.HandlerFunc(notfound)
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
+	http.Handle("/", r)
+	//r.HandleFunc("/notfound", notfound)
+	fmt.Println("Your website is now online.......")
 	http.ListenAndServe(":3000", nil)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "home.html", nil)
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "contact.html", nil)
+}
+
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	templates.ExecuteTemplate(w, "faq.html", nil)
+}
+
+func notfound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	templates.ExecuteTemplate(w, "404page.html", nil)
 }
